2444f0bd63c9ce49e6059d37434f9844bb6e2f0e
xilas <dahepix1@gmail.com>
2025-09-12 09:31:06 +0200

    added tex and c# project

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e8fc98dbb6392cb91e9037c52b3812701acebfc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5e138f07c77117a916b9e6573061292018c3792")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
