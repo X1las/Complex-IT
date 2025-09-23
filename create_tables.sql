f5cea30f67fc779609cb6ef533fe1563109c39dd
xilas <dahepix1@gmail.com>
2025-09-23 17:08:56 +0200

    updates to portfolio project

---
+++ create_tables.sql
@@ -0,0 +1,30 @@
+CREATE OR REPLACE users (
+    id 
+);
+CREATE OR REPLACE user_history (
+    id 
+);
+CREATE OR REPLACE user_ratings (
+    id 
+);
+CREATE OR REPLACE bookmarks (
+    id 
+);
+CREATE OR REPLACE users (
+    id 
+);
+CREATE OR REPLACE titles (
+    id 
+);
+CREATE OR REPLACE timeslot (
+    id 
+);
+CREATE OR REPLACE movie_ratings (
+    id 
+);
+CREATE OR REPLACE attends (
+    id 
+);
+CREATE OR REPLACE crew (
+    id 
+);
