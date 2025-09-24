843577f12739340ba78ef875cbc5974cd9a43999
xilas <dahepix1@gmail.com>
2025-09-24 15:54:04 +0200

    updated tables

--- create_tables.sql
+++ create_tables.sql
@@ -1,40 +1,29 @@
 DROP TABLE IF EXISTS users CASCADE;
-
 DROP TABLE IF EXISTS user_history CASCADE;
-
 DROP TABLE IF EXISTS user_ratings CASCADE;
-
 DROP TABLE IF EXISTS bookmarks CASCADE;
-
 DROP TABLE IF EXISTS titles CASCADE;
-
 DROP TABLE IF EXISTS movie_ratings CASCADE;
-
 DROP TABLE IF EXISTS attends CASCADE;
-
 DROP TABLE IF EXISTS crew CASCADE;
-
 DROP TABLE IF EXISTS alternate_titles CASCADE;
-
 DROP TABLE IF EXISTS series CASCADE;
-
 DROP TABLE IF EXISTS episodes CASCADE;
-
 DROP TABLE IF EXISTS games CASCADE;
-
 DROP TABLE IF EXISTS runtimes CASCADE;
-
 DROP TABLE IF EXISTS boxoffice CASCADE;
-
 DROP TABLE IF EXISTS dvds CASCADE;
-
 DROP TABLE IF EXISTS productions CASCADE;
-
 DROP TABLE IF EXISTS posters CASCADE;
-
 DROP TABLE IF EXISTS websites CASCADE;
-
 DROP TABLE IF EXISTS maturity_ratings CASCADE;
+DROP TABLE IF EXISTS genres CASCADE;
+DROP TABLE IF EXISTS title_genres CASCADE;
+DROP TABLE IF EXISTS title_awards CASCADE;
+DROP TABLE IF EXISTS languages CASCADE;
+DROP TABLE IF EXISTS title_languages CASCADE;
+DROP TABLE IF EXISTS region CASCADE;
+DROP TABLE IF EXISTS title_region CASCADE;
 
 CREATE TABLE users (
     username VARCHAR(16) NOT NULL,
@@ -47,16 +36,12 @@ CREATE TABLE titles (
     id VARCHAR(12) NOT NULL,
     title VARCHAR(256),
     titletype VARCHAR(20),
-    genres VARCHAR(80),
     plot TEXT,
     year VARCHAR(100),
     startyear VARCHAR(4),
     endyear VARCHAR(4),
     release_date VARCHAR(80),
     originaltitle TEXT,
-    awards VARCHAR(80),
-    language TEXT,
-    country VARCHAR(256),
     isadult BOOLEAN,
     PRIMARY KEY (id)
 );
@@ -119,6 +104,11 @@ CREATE TABLE attends (
     FOREIGN KEY (crew_id) REFERENCES crew (id)
 );
 
+CREATE TABLE region (
+    region VARCHAR (10) NOT NULL,
+    PRIMARY KEY (region)
+);
+
 CREATE TABLE alternate_titles (
     title_id VARCHAR(10) NOT NULL,
     alts_ordering INT NOT NULL,
@@ -126,8 +116,10 @@ CREATE TABLE alternate_titles (
     region VARCHAR(10),
     title_language VARCHAR(10),
     types VARCHAR(80),
+    isoriginaltitle BOOLEAN,
     PRIMARY KEY (title_id, alts_ordering),
-    FOREIGN KEY (title_id) REFERENCES titles (id)
+    FOREIGN KEY (title_id) REFERENCES titles (id),
+    FOREIGN KEY (region) REFERENCES region (region)
 );
 
 CREATE TABLE series (
@@ -149,7 +141,7 @@ CREATE TABLE episodes (
 );
 
 CREATE TABLE runtimes (
-    runtime_id VARCHAR(10) NOT NULL,
+    title_id VARCHAR(10) NOT NULL,
     runtime VARCHAR(80) NOT NULL,
     PRIMARY KEY (runtime_id, runtime),
     FOREIGN KEY (runtime_id) REFERENCES titles (id)
@@ -196,6 +188,48 @@ CREATE TABLE maturity_ratings (
     PRIMARY KEY (title_id),
     FOREIGN KEY (title_id) REFERENCES titles (id)
 );
+
+CREATE TABLE genres (
+    genre VARCHAR(30) NOT NULL,
+    PRIMARY KEY (genre)
+);
+
+CREATE TABLE title_genres (
+    title_id VARCHAR(10) NOT NULL,
+    genre VARCHAR(30) NOT NULL,
+    PRIMARY KEY (title_id, genre),
+    FOREIGN KEY (title_id) REFERENCES titles (id),
+    FOREIGN KEY (genre) REFERENCES genres (genre)
+)
+
+CREATE TABLE title_awards (
+    title_id VARCHAR(10) NOT NULL,
+    awards VARCHAR(64),
+    PRIMARY KEY (title_id),
+    FOREIGN KEY (title_id) REFERENCES titles (id)
+);
+
+CREATE TABLE languages (
+    language VARCHAR(30) NOT NULL,
+    PRIMARY KEY (language)
+);
+
+CREATE TABLE title_languages (
+    title_id VARCHAR(10) NOT NULL,
+    language VARCHAR(30) NOT NULL,
+    PRIMARY KEY (title_id, language),
+    FOREIGN KEY (title_id) REFERENCES titles (id),
+    FOREIGN KEY (language) REFERENCES languages (language)
+);
+
+CREATE TABLE title_region (
+    title_id VARCHAR (10) NOT NULL,
+    region VARCHAR (10) NOT NULL,
+    PRIMARY KEY (title_id, region),
+    FOREIGN KEY (title_id) REFERENCES titles (id),
+    FOREIGN KEY (region) REFERENCES region (region)
+);
+
 
 DROP TABLE IF EXISTS temp_basics;
 
@@ -251,5 +285,69 @@ INSERT INTO crew
 SELECT DISTINCT
     nconst as id,
     primaryname as full_name,
-    birthyear
+    birthyear,
+    deathyear
 FROM name_basics;
+
+INSERT INTO titles
+SELECT
+    temp_basics.id,
+    temp_basics.title,
+    temp_basics.titletype,
+    plot,
+    year,
+    startyear,
+    endyear,
+    release_date,
+    originaltitle,
+    isadult
+FROM temp_basics
+LEFT JOIN temp_omdb 
+ON temp_basics.id = temp_omdb.id;
+
+INSERT INTO websites
+SELECT 
+    tconst as title_id
+    website
+FROM temp_omdb
+where website != 'N/A';
+
+INSERT INTO dvds
+SELECT DISTINCT
+    title_id,
+    dvd
+FROM omdb_data
+
+INSERT INTO episodes
+SELECT DISTINCT
+    tconst,
+    parenttconst,
+    seasonnumber,
+    episodenumber
+FROM title_episode
+
+INSERT INTO boxoffice
+SELECT DISTINCT
+    title_id,
+    boxoffice
+FROM omdb_data
+
+INSERT INTO alternate_titles
+SELECT 
+    tconst as id,
+    ordering as alts_ordering,
+    title as alts_title,
+    region,
+    language as title_language,
+    types
+FROM title_akas;
+
+SELECT attributes
+FROM title_akas
+WHERE attributes IS NOT NULL AND attributes != '';
+
+--INSERT INTO genres lave en do bock med forloob--
+
+insert INTO runtimes
+SELECT title_id, runtime
+FROM temp_basics;
