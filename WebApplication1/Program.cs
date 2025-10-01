1dd099350c7fc96215532bfcff85d58cc6d1436f
xilas <dahepix1@gmail.com>
2025-10-01 11:19:03 +0200

    edited c# code

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,1 +1,10 @@
+using System;
 
+string? s1 = null;
+string? s2 = s1 ?? "Default Value";
+string? s3 = s1 ??= "Default Value";
+
+public static num int operator +(num a, num b)
+{
+    return a + b;
+}
--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4590f80d7ab82ca93117027637c66c88de12751b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a14e3c2870d48798a6fec7089a1d152752a1030")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
