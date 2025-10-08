45c698819d3d1e14d6b3ec3ba9291f26d2f135bc
xilas <dahepix1@gmail.com>
2025-10-08 13:45:13 +0200

    initial branch commit

--- WebApplication1/obj/Release/net9.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Release/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c76f29efce4f9f3972edcc1ff939d1ab4cb590c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a18ee12b451c5a8c14fe6519a689c57d85a26118")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
