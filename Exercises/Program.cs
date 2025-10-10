68f327888ff98ae1166fe39a9420ebb71d7abe4c
xilas <dahepix1@gmail.com>
2025-10-10 09:56:38 +0200

    Attempted Npgsql connection

--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using Npgsql;
+
+var connectionString = "Host=newtlike.com;Port=5432;Database=rucdb;Username=rucdb;Password=testdb;Trust Server Certificate=true";
+
+try
+{
+    var connection = new NpgsqlConnection(connectionString);
+    connection.Open();
+
+    Console.WriteLine("Connected to the database successfully.");
+
+    connection.Close();
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error connecting to the database: {ex.Message}");
+}
+
+
+Thread.Sleep(1000);
+
+Console.WriteLine("Connection closed.");
--- WebAppTest/obj/Debug/net9.0/WebAppTest.AssemblyInfo.cs
+++ WebAppTest/obj/Debug/net9.0/WebAppTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d8cbb77f259d76f1d1e0bc3350a48f36990232e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5561d735df9c8e7f30df1c6065549252857cc64")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
