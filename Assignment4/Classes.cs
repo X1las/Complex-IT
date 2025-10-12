ce893aee0f41152e41810e714b79a55fadf68a79
xilas <dahepix1@gmail.com>
2025-10-12 14:06:15 +0200

    added initial getters, setters and empty placeholder methods

--- Assignment4/Classes.cs
+++ Assignment4/Classes.cs
@@ -16,9 +16,42 @@ public class Product
     public string? Description { get; set; }
     public decimal Price { get; set; }
     public int CategoryId { get; set; }
+
+    public int UnitPrice { get; set; }
+    public int UnitsInStock { get; set; }
+    public string? QuantityPerUnit { get; set; }
+    public Category? Category { get; set; }
 }
 
 public class DataService
 {
+    public List<Category> Categories { get; set; } = new List<Category>();
+    public List<Product> Products { get; set; } = new List<Product>();
 
+    public List<Category> GetCategories()
+    {
+        return Categories;
+    }
+    public Category? GetCategory(int id)
+    {
+        return Categories.FirstOrDefault(c => c.Id == id);
+    }
+    public Category CreateCategory(string name, string description)
+    {
+        
+    }
+    public bool DeleteCategory(int id)
+    {
+
+    }
+
+    public bool UpdateCategory(int id, string name, string description)
+    {
+
+    }
+
+    public Product? GetProduct(int id)
+    {
+
+    }
 }
