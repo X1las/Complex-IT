8eeba8d2327d4433e8a262c0ccba36cf2a8b06be
xilas <dahepix1@gmail.com>
2025-10-14 13:16:05 +0200

    rebuilt project

--- Assignment4/obj/Release/net9.0/Assignment4.AssemblyInfo.cs
+++ Assignment4/obj/Release/net9.0/Assignment4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4532886bac1caad74aff530a732fb1f24b294213")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d85fc92b88811b9e85d32abc2d068e9a0a915d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
