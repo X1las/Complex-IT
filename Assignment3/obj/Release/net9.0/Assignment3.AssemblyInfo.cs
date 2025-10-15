efa7913a905239be2b7474c76dab3f11f741bf0c
xilas <dahepix1@gmail.com>
2025-10-15 13:44:49 +0200

    removed reduntant files

--- Assignment3/obj/Release/net9.0/Assignment3.AssemblyInfo.cs
+++ Assignment3/obj/Release/net9.0/Assignment3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5561d735df9c8e7f30df1c6065549252857cc64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+709445320f12d826761a96e8f1ad35e875623b04")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Exercises/obj/Release/net9.0/Exercises.AssemblyInfo.cs
+++ Exercises/obj/Release/net9.0/Exercises.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercises")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5561d735df9c8e7f30df1c6065549252857cc64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+709445320f12d826761a96e8f1ad35e875623b04")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercises")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercises")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
