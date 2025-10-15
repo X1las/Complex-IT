8b1eb33adab7bcc794cf5f61553fa80d4c770f20
xilas <dahepix1@gmail.com>
2025-10-15 14:04:52 +0200

    added bulskov files for assignment 4 part 2

--- Assignment4Tests/obj/Release/net9.0/Assignment4Tests.AssemblyInfo.cs
+++ Assignment4Tests/obj/Release/net9.0/Assignment4Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment4Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e42d5280fdcdec857c00997720ad63fa8b93ade3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dc9aaa6dd22157f0c9f78d1181d950981a974df")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment4Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment4Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
