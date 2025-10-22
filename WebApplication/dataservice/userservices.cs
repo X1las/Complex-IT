0a70238e714a71d029a7b8eb18d1ac6599aba26c
xilas <dahepix1@gmail.com>
2025-10-22 19:12:17 +0200

    splitting and deleting files

---
+++ WebApplication/dataservice/userservices.cs
@@ -0,0 +1,87 @@
+
+namespace DataServiceLayer;
+
+public class UserDataService
+{
+    public Users? GetUserByUsername(ImdbContext context, string username)
+    {
+        return context.User.FirstOrDefault(u => u.Username == username);
+    }
+
+    public bool CreateUser(ImdbContext context, Users newUser)
+    {
+        try
+        {
+            context.User.Add(new Users { Username = newUser.Username, Pswd = newUser.Pswd });
+            context.SaveChanges();
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+
+    public List<UserRatings>? GetUserRatingsByUsername(ImdbContext context, string username)
+    {
+        return context.UsersRating
+            .Where(ur => ur.Username == username)
+            .ToList();
+    }
+
+    public bool CreateUserRating(ImdbContext context, UserRatings rating)
+    {
+        try
+        {
+            context.UsersRating.Add(new UserRatings { Username = rating.Username, TitleId = rating.TitleId, Rating = rating.Rating });
+            context.SaveChanges();
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+
+    public List<Bookmarks>? GetUserBookmarksByUsername(ImdbContext context, string username)
+    {
+        return context.Bookmark
+            .Where(b => b.Username == username)
+            .ToList();
+    }
+
+    public bool CreateUserBookmark(ImdbContext context, Bookmarks bookmark)
+    {
+        try
+        {
+            context.Bookmark.Add(new Bookmarks { Username = bookmark.Username, TitleId = bookmark.TitleId });
+            context.SaveChanges();
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+
+    public List<UserHistory>? GetUserHistoryByUsername(ImdbContext context, string username)
+    {
+        return context.UsersHistory
+            .Where(uh => uh.Username == username)
+            .ToList();
+    }
+
+    public bool CreateUserHistory(ImdbContext context, UserHistory history)
+    {
+        try
+        {
+            context.UsersHistory.Add(new UserHistory { Username = history.Username, TitleId = history.TitleId, Date = history.Date });
+            context.SaveChanges();
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+}
