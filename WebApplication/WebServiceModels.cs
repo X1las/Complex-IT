01ebd59aa00e8e408059cf856dd57e2e0e53837f
xilas <dahepix1@gmail.com>
2025-10-22 12:33:23 +0200

    changed webservicemodel

--- WebApplication/WebServiceModels.cs
+++ WebApplication/WebServiceModels.cs
@@ -1,3 +1,5 @@
+using DataServiceLayer;
+
 namespace WebServiceLayer;
 
 // Data transfer objects (DTOs) for Web Service Layer
@@ -11,45 +13,16 @@ public class TitleModel
     public string Url { get; set; } = string.Empty;
 }
 
-public class TitleDetails // TitleDetails DTO
-{
-    public string Id { get; set; } = string.Empty;
-    public string Title { get; set; } = string.Empty;
-    public string? TitleType { get; set; }
-    public string? Plot { get; set; }
-    public string Year { get; set; } = string.Empty;
-    public string StartYear { get; set; } = string.Empty;
-    public string? EndYear { get; set; }
-    public string? Release_Date { get; set; }
-    public string? OriginalTitle { get; set; }
-    public bool IsAdult { get; set; }
-    public double Rating { get; set; }
-    public int Votes { get; set; }
-    public string Url { get; set; } = string.Empty;
-}
-
 public class UserModel // UserModel DTO
 {
     public string Username { get; set; } = string.Empty;
-    public string Url { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public List<Bookmarks> Bookmarks { get; set; } = new();
+    public List<UserRatings> Ratings { get; set; } = new();
+    public List<UserHistory> History { get; set; } = new();
 }
 
-public class CreateUser // CreateUser DTO
-{
-    public string Username { get; set; } = string.Empty;
-    public string Pswd { get; set; } = string.Empty;
-    public List<UserRatings>? UserRatings { get; set; } = new List<UserRatings>();
-    public List<UserHistory>? UserHistory { get; set; } = new List<UserHistory>();
-    public List<Bookmarks>? Bookmarks { get; set; } = new List<Bookmarks>();
-}
-
-public class UserLogin // UserLogin DTO
-{
-    public string Username { get; set; } = string.Empty;
-    public string Pswd { get; set; } = string.Empty;
-}
-
-public class CrewModelDetails // CrewModelDetails DTO
+public class CrewModel // CrewModelDetails DTO
 {
     public string CrewId { get; set; } = string.Empty;
     public string Fullname { get; set; } = string.Empty;
@@ -59,17 +32,4 @@ public class CrewModelDetails // CrewModelDetails DTO
     public string Url { get; set; } = string.Empty;
 }
 
-public class BookmarkModel
-{
-    public string Username { get; set; } = string.Empty;
-    public string TitleId { get; set; } = string.Empty;
-    public string TitleName { get; set; } = string.Empty;
-    public string? Rating { get; set; }
-    public string Url { get; set; } = string.Empty;
-}
 
-public class CreateBookmark //CreateBookmarks DTO
-{
-    public string TitleId { get; set; } = string.Empty;
-    public string Username {get; set;} = string.Empty;
-}
