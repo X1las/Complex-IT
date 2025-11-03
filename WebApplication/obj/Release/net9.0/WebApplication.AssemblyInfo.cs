896faae66e7b36c4b89512c03c943c91157f1229
xilas <dahepix1@gmail.com>
2025-11-03 16:50:13 +0100

    pre-pull commit

--- WebApplication/obj/Release/net9.0/WebApplication.AssemblyInfo.cs
+++ WebApplication/obj/Release/net9.0/WebApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ec5e40060354f7e2f469d91a81b0098a7fc3183")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c4a6e04f09df29fbec3fb7ec71fe02cca1e0ce7")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
