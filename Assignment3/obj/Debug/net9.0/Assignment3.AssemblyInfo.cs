2c6306df913f1f1ccef158adb63640ecb8cbd35b
xilas <dahepix1@gmail.com>
2025-11-07 11:12:00 +0100

    more assembly

--- Assignment3/obj/Debug/net9.0/Assignment3.AssemblyInfo.cs
+++ Assignment3/obj/Debug/net9.0/Assignment3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1337f3e78a67ea4e878305b61b0fa7a7bbb65314")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c09385474a492cb03fa2c4006482091945bb9ddd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
