8c71625cd03988832b99bd557237dcc53fa0702d
xilas <dahepix1@gmail.com>
2025-11-18 14:08:31 +0100

    fetching images

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -5,14 +5,18 @@ import './App.css'
 function App({ count }) {
   const [users, setUsers] = useState([]);
   useEffect(() => {
-    fetch("https://api.themoviedb.org/3/search/person?query=david&api_key=f7cb406cd3ce48761cb8749ec2be8e89")
+    fetch("https://api.themoviedb.org/3/search/person?query=anna&api_key=f7cb406cd3ce48761cb8749ec2be8e89")
       .then(res => res.json())
       .then(data => setUsers(data.results));
   }, [count]);
+  console.log(users);
   return (
     <div>
       {users.map((u, index) => (
-        <p key={index}>{u.id}</p>
+        <div key={index}>
+          <img src={`https://image.tmdb.org/t/p/original${u.profile_path}`}/>
+          <p>{u.name} , {u.id}</p>
+        </div>
       ))}
     </div>
   );
