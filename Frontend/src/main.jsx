348e4fc727a202fcd5cfc56220a5c4a932d8a536
xilas <dahepix1@gmail.com>
2025-11-26 14:28:40 +0100

    updated index with routes

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,38 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {BrowserRouter,Routes, Route} from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import Profile from './routes/profile.jsx'
+import Bookmarks from './routes/Bookmarks.jsx'
+import History from './routes/History.jsx'
+import Ratings from './routes/Ratings.jsx'
 
-createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById('root'))
+root.render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          
+          <Route path="profile/:id/" element={<Profile/>}>
+            <Route path="bookmarks" element={<Bookmarks/>} />
+            <Route path="history" element={<History/>} />
+            <Route path="ratings" element={<Ratings/>} />
+          </Route>
+
+          <Route path="login" element={<Login />}>
+            <Route path="Register" element={<Register />} />
+          </Route>
+          
+          <Route path="person/:id" element={<Person />} />
+          <Route path="title/:id" element={<Title />} />
+          <Route path="search" element={<Search />} />
+
+          <Route path="*" element={<div>404 Not Found</div>} />
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
