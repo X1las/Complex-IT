dbe52b52061acb9c027cd093169acf8fbc326aff
Ratagal <Malthe@tranberg-larsen.dk>
2025-11-28 10:01:49 +0100

    home

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import {BrowserRouter,Routes, Route} from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import Home from './home.jsx'
 import Profile from './routes/profile.jsx'
 import Bookmarks from './routes/Bookmarks.jsx'
 import History from './routes/History.jsx'
@@ -13,10 +14,12 @@ root.render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />}>
+        <Route path="/" element={<Home />}>
           
-          <Route path="profile/:id/" element={<Profile/>}>
-            <Route path="bookmarks" element={<Bookmarks/>} />
+           <Route path="profile/:id/" element={<Profile/>}>
+           
+        </Route>
+          {/*  <Route path="bookmarks" element={<Bookmarks/>} />
             <Route path="history" element={<History/>} />
             <Route path="ratings" element={<Ratings/>} />
           </Route>
@@ -27,7 +30,7 @@ root.render(
           
           <Route path="person/:id" element={<Person />} />
           <Route path="title/:id" element={<Title />} />
-          <Route path="search" element={<Search />} />
+          <Route path="search" element={<Search />} /> */}
 
           <Route path="*" element={<div>404 Not Found</div>} />
 
