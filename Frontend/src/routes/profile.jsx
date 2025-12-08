43b2a1f6e5d982aa6dfd67482cf9d202e415ced2
Ratagal <mto@tranberg-larsen.dk>
2025-12-08 17:29:45 +0100

    profile css in progress

--- Frontend/src/routes/profile.jsx
+++ Frontend/src/routes/profile.jsx
@@ -1,9 +1,7 @@
-import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import '../css/profile.css';
 import { Link, Outlet } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext.jsx';
-import {useAddBookmarks} from './bookmarks.jsx';
 import {useBookmarks} from './bookmarks.jsx';
 import {useRemoveBookmark} from './bookmarks.jsx';
 /* 
@@ -45,23 +43,26 @@ const Profile = () => {
             <button className='btnProfile' onClick={handleLogout}> <Link to={`/`}>logout</Link></button>
            </div>
         </div>
-        <div className='bookmarkscontainer'>
-    <div className='container'>
+        <div className='contentContainer'>
+        <div className='bookmarksContainer'>
+    <div className=''>
       <h2>Bookmarks</h2>
       {(!bookmarks || bookmarks.length === 0) ? (<p>You don't have any Bookmarks.</p>) : (
         bookmarks.map(item => (
-        <div key={item.titleId + item.viewedAt} className='posterContainer'>
-          <img src={item.posterUrl} alt={item.title} className="historyPoster" />
-          <div >
-            <Link to={`/title/${item.titleId}`}><h3>{item.title}</h3></Link>
-            <button onClick={() => {removeBookmark(item.titleId);}}>Remove</button>
+        <div key={item.titleId + item.viewedAt} className='bookmarkPosterContainer'>
+          <img src={item.posterUrl} alt={item.title} className="bookmarkPoster" />
+          <div>
+            <Link to={`/title/${item.titleId}`}><h3 className='bookmarkTitle'>{item.title}</h3></Link>
+            <button className='removeBtn' onClick={() => {removeBookmark(item.titleId);}}>Remove</button>
           </div>
         </div>
      ))
       )}
     </div>
         </div>
-      <Outlet />
+        
+      <div className="outlet"><Outlet/></div>
+    </div>
     </div>
   );
 };
