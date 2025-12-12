b5aaabcf86e28620f48edf3d2bd46a465373028e
xilas <dahepix1@gmail.com>
2025-12-12 13:05:49 +0100

    reworked some naming and redid person.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,50 +1,48 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 import logo from './assets/image.png';
-import icon from './assets/icon.png'; 
+import icon from './assets/icon.png';
 import { Link, Outlet, useNavigate } from 'react-router-dom';
 import { useAuth } from './context/AuthContext.jsx';
-
-
 
 function App() {
 
   const [search, setSearch] = useState('');
   const navigate = useNavigate();
-  const {user} = useAuth();
-      const { logout } = useAuth();
-    const handleLogout = () => {logout();};
+  const { user } = useAuth();
+  const { logout } = useAuth();
+  const handleLogout = () => { logout(); };
 
   /* console.log('Current user in App component:', user); */
   const onSearchSubmit = (e) => {
     e.preventDefault();
     if (!search.trim()) return;
-    navigate(`/search/${(search)}`);
+    navigate(`/search/${(search)}`);                                                                                                                   
   }
 
   return (
     <>
-        <nav>
-            <div ><Link to="/"><img className='imgPlaceholder' src={logo} alt="Logo" /></Link></div>
-            <form onSubmit={onSearchSubmit} style={{display:'inline'}}> 
-              <input className='searchField' placeholder={`${search}`} type="text" value={search} onChange={e => setSearch(e.target.value)} />
-          </form>
-            
-          <div className='profileholder'>
-              <Link to={user ? `/profile/${user.username}` : `/login`}> 
-                <img className='profileIcon' src={icon} alt="profilePic" />
-              </Link>            
-            
-          </div>
-          
-          <div className='btnLogout'>
-            {(!user) ? (<p></p>) : (
-              <button className='Logout'onClick={handleLogout}> <Link to={`/`}>Logout</Link></button>
+      <nav>
+        <div ><Link to="/"><img className='imgPlaceholder' src={logo} alt="Logo" /></Link></div>
+        <form onSubmit={onSearchSubmit} style={{ display: 'inline' }}>
+          <input className='searchField' placeholder={`${search}`} type="text" value={search} onChange={e => setSearch(e.target.value)} />
+        </form>
+
+        <div className='profileholder'>
+          <Link to={user ? `/profile/${user.username}` : `/login`}>
+            <img className='profileIcon' src={icon} alt="profilePic" />
+          </Link>
+
+        </div>
+
+        <div className='btnLogout'>
+          {(!user) ? (<p></p>) : (
+            <button className='Logout' onClick={handleLogout}> <Link to={`/`}>Logout</Link></button>
           )}
-              </div>
-          
-        </nav>
-        <Outlet />
+        </div>
+
+      </nav>
+      <Outlet />
     </>
   )
 }
