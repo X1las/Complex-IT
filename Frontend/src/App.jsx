bd433213c1a19cd8e1ab45fbbf389c074f2a421c
Ratagal <mto@tranberg-larsen.dk>
2025-12-15 15:11:27 +0100

    bootstrap navbar med css fordi bootstrap er lowkey shit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,16 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css' 
 import logo from './assets/image.png';
 import icon from './assets/icon.png';
 import { Link, Outlet, useNavigate } from 'react-router-dom';
 import { useAuth } from './context/AuthContext.jsx';
+import Container from 'react-bootstrap/Container';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import Dropdown from 'react-bootstrap/Dropdown';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
 
@@ -22,26 +29,54 @@ function App() {
 
   return (
     <>
-      <nav>
-        <div ><Link to="/"><img className='imgPlaceholder' src={logo} alt="Logo" /></Link></div>
-        <form onSubmit={onSearchSubmit} style={{ display: 'inline' }}>
-          <input className='searchField' placeholder={`${search}`} type="text" value={search} onChange={e => setSearch(e.target.value)} />
-        </form>
+      <Navbar fixed="top" className="navbar" >
 
-        <div className='profileholder'>
-          <Link to={user ? `/profile/${user.username}` : `/login`}>
-            <img className='profileIcon' src={icon} alt="profilePic" />
-          </Link>
+          <Container fluid className="justify-content-evenly" >
 
-        </div>
+          <Navbar.Brand as={Link} to="/" className="d-flex align-items-center">
+          
+            <img src={logo} className="logo" alt="Logo" />
+          </Navbar.Brand>
 
-        <div className='btnLogout'>
-          {(!user) ? (<p></p>) : (
-            <button className='Logout' onClick={handleLogout}> <Link to={`/`}>Logout</Link></button>
-          )}
-        </div>
+          
+          <Form onSubmit={onSearchSubmit} className="flex-grow-1 mx-3" >
 
-      </nav>
+            <Row className="g-2">
+
+              <Col style={{  justifyContent: "center", display: "flex" }}>
+
+                <Form.Control
+                  className="form-control searchField"
+                  placeholder="Search"
+                  style={{color:'white'}}
+                  type="search"
+                  aria-label="Search"
+                  value={search}
+                  onChange={e => setSearch(e.target.value)}
+                />
+              </Col>
+
+            </Row>
+
+          </Form>
+
+          <Nav >
+
+            <NavDropdown menuVariant="dark" title={<img src={icon} alt="profilePic" />} style={{height:"90px", width:"80px", padding:"10px", marginLeft:" 80px"}} id="nav-dropdown" align="end" >
+                   
+              <NavDropdown.Item  as={Link} to={user ? `/profile/${user.username}` : `/login`} ><button style={{width:"100%", margin:0}} className="btn btn-outline-warning" >Your Page</button></NavDropdown.Item>
+                { !user ? null:
+                <NavDropdown.Item ><button style={{width:"100%", margin:0}} className="btn btn-outline-warning"  onClick={handleLogout}>Logout</button></NavDropdown.Item>
+                }
+              <NavDropdown.Item as={Link} to={`/`} ><button style={{width:"100%", margin:0}} className="btn btn-outline-warning">Andet?</button></NavDropdown.Item>
+
+            </NavDropdown>
+          </Nav>
+
+        </Container>
+
+      </Navbar>
+
       <Outlet />
     </>
   )
